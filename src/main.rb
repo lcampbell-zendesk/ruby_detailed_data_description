1c0ee60e92c97741c342fdafa1462e440bde251e
Logan Campbell <colin@logaan.net>
2021-12-09 04:21:22 +1100

    load into table

--- src/main.rb
+++ src/main.rb
@@ -295,8 +295,8 @@ ORGANIZATION_FIELDS = [
   array_of_strings('tags')
 ]
 
-TICKETS = DataSource.new('tickets', TICKET_FIELDS, 'data/tickets.json').execute
-USERS = DataSource.new('users', USER_FIELDS, 'data/users.json').execute
-ORGANIZATIONS = DataSource.new('organizations', ORGANIZATION_FIELDS, 'data/organizations.json').execute
+TICKETS = DataSource.new('tickets', TICKET_FIELDS, 'data/tickets.json').load_into_table
+USERS = DataSource.new('users', USER_FIELDS, 'data/users.json').load_into_table
+ORGANIZATIONS = DataSource.new('organizations', ORGANIZATION_FIELDS, 'data/organizations.json').load_into_table
 
 DATABASE = Database.new([TICKETS, USERS, ORGANIZATIONS])
